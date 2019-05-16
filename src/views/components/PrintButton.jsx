8506b3e19e952d5fc541d7fd64a07e9e295e6f1e
Robbert Broersma <mail@robbertbroersma.nl>
2019-05-16 14:00:37 +0200

    Avoid using dangerouslySetInnerHTML

--- src/views/components/PrintButton.jsx
+++ src/views/components/PrintButton.jsx
@@ -2,19 +2,20 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 const PrintButton = ({ title, type, disabled }) => {
-    const markup = ( _title, _type, _disabled ) => {
-        return { __html: '<button ' + (_disabled ? 'disabled="disabled"' : '') + ' type="' + (_type ? _type : 'button') + '" class="PrintButton__btn" href="#" onclick="window.print();">' + _title + '</button>' };
-    };
-
+    /* eslint-disable no-script-url */
     return (
-        <div className="PrintButton" dangerouslySetInnerHTML={markup(title, type, disabled)}/>
+        <div className="PrintButton">
+            <a href="javascript:print()" role="button" className="PrintButton__btn">
+                {title}
+            </a>
+        </div>
     );
 };
 
 PrintButton.propTypes = {
     title: PropTypes.string,
     type: PropTypes.string,
-    disabled: PropTypes.bool
+    disabled: PropTypes.bool,
 };
 
 module.exports = PrintButton;
