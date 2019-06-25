Amsterdam/fixxx-pakjekraam

commit c480354d306267318f4ce74e69f9003cdb9931a3

Meervoudige uitbreiding

src/pakjekraam-api.js
@@ -50,6 +50,8 @@ const getOndernemerVoorkeuren = erkenningsNummer => {
     });
 };
 
+const getMarktProperties = marktId => loadJSON(`./data/${slugifyMarkt(marktId)}/markt.json`, []);
+
 const getBranches = marktId => loadJSON(`./data/${slugifyMarkt(marktId)}/branches.json`, []);
 
 const getMarktplaatsen = marktId => loadJSON(`./data/${slugifyMarkt(marktId)}/locaties.json`, []);
@@ -60,6 +62,7 @@ const getMarktGeografie = marktId => loadJSON(`./data/${slugifyMarkt(marktId)}/g
 
 const getIndelingslijstInput = (token, marktId, date) =>
     Promise.all([
+        getMarktProperties(marktId),
         getMarktondernemersByMarkt(token, marktId).then(ondernemers =>
             ondernemers
                 .filter(ondernemer => !ondernemer.doorgehaald)
@@ -76,6 +79,9 @@ const getIndelingslijstInput = (token, marktId, date) =>
                         id: erkenningsnummer,
                         erkenningsNummer: erkenningsnummer,
                         plaatsen: data.vastePlaatsen,
+                        voorkeur: {
+                            aantalPlaatsen: Math.max(1, data.aantal3MeterKramen + data.aantal4MeterKramen),
+                        },
                         sollicitatieNummer,
                         status,
                     };
@@ -90,7 +96,18 @@ const getIndelingslijstInput = (token, marktId, date) =>
         getMarkt(token, marktId),
         getALijst(token, marktId, date),
     ]).then(args => {
-        const [ondernemers, locaties, aanmeldingen, voorkeuren, branches, paginas, geografie, markt, aLijst] = args;
+        const [
+            marktProperties,
+            ondernemers,
+            locaties,
+            aanmeldingen,
+            voorkeuren,
+            branches,
+            paginas,
+            geografie,
+            markt,
+            aLijst,
+        ] = args;
 
         const marktplaatsen = locaties.map(locatie => ({
             plaatsId: locatie.plaatsId,
@@ -99,6 +116,7 @@ const getIndelingslijstInput = (token, marktId, date) =>
         }));
 
         return {
+            ...marktProperties,
             locaties,
             aanmeldingen,
             voorkeuren,

src/indeling-scenario.model.ts
@@ -18,6 +18,7 @@ export interface IMarktScenario {
     aLijst: IMarktondernemer[];
     branches: IBranche[];
     rows: IMarktplaats[][];
+    expansionLimit?: number;
 }
 
 export interface IMarktScenarioStub {
@@ -28,6 +29,7 @@ export interface IMarktScenarioStub {
     aLijst?: IMarktondernemer[];
     branches?: IBranche[];
     rows?: string[][];
+    expansionLimit?: number;
 }
 
 export interface IMarktplaatsStub {

src/indeling-scenario.ts
@@ -148,6 +148,7 @@ const marktScenario = (callback: (utils: scenarioUtils) => IMarktScenarioStub): 
         voorkeuren: seed.voorkeuren || [],
         aLijst: seed.aLijst || [],
         branches: seed.branches || [],
+        expansionLimit: seed.expansionLimit,
     };
 
     const markt: IMarktScenario = {

src/indeling.test.js
@@ -5,6 +5,7 @@ const { marktScenario } = require('./indeling-scenario.ts');
 
 const FIRST_CHOICE = Number.MAX_SAFE_INTEGER;
 const SECOND_CHOICE = FIRST_CHOICE - 1;
+const THIRD_CHOICE = FIRST_CHOICE - 2;
 
 describe('Automatisch toewijzen marktplaatsen', () => {
     it('Een ondernemer wordt toegewezen aan een lege plek', () => {
@@ -75,7 +76,7 @@ describe('Automatisch toewijzen marktplaatsen', () => {
         expect(indeling.toewijzingen[0].plaatsen.sort()).toStrictEqual(['1', '2']);
     });
 
-    it.skip('Een ondernemer kan uitbreiden naar een meervoudige plaats', () => {
+    it('Een ondernemer kan uitbreiden naar een meervoudige plaats', () => {
         /*
          * Scenario:
          * - 3 marktplaatsen
@@ -90,6 +91,29 @@ describe('Automatisch toewijzen marktplaatsen', () => {
 
         expect(indeling.toewijzingen.length).toBe(1);
         expect(indeling.toewijzingen[0].plaatsen.sort()).toStrictEqual(['1', '2', '3']);
+    });
+
+    it('Een ondernemer kan niet verder vergroten dan is toegestaan', () => {
+        /*
+         * Scenario:
+         * - 3 marktplaatsen
+         * - 1 ondernemer met een meervoudige plaats
+         */
+        const markt = marktScenario(({ ondernemer, marktplaats, voorkeur }) => ({
+            ondernemers: [ondernemer({ voorkeur: { aantalPlaatsen: 3 } })],
+            marktplaatsen: [marktplaats(), marktplaats(), marktplaats()],
+            voorkeuren: [
+                voorkeur({ sollicitatieNummer: 1, plaatsId: '2', priority: FIRST_CHOICE }),
+                voorkeur({ sollicitatieNummer: 1, plaatsId: '3', priority: SECOND_CHOICE }),
+                voorkeur({ sollicitatieNummer: 1, plaatsId: '1', priority: THIRD_CHOICE }),
+            ],
+            expansionLimit: 2,
+        }));
+
+        const indeling = calcToewijzingen(markt);
+
+        expect(indeling.toewijzingen.length).toBe(1);
+        expect(indeling.toewijzingen[0].plaatsen.sort()).toStrictEqual(['2', '3']);
     });
 
     it('Een ondernemer met te laag ancenniteitsnummer krijgt geen dagvergunning op een volle markt', () => {
@@ -374,6 +398,44 @@ describe('Automatisch toewijzen marktplaatsen', () => {
                 .find(({ ondernemer: { sollicitatieNummer } }) => sollicitatieNummer === 2)
                 .plaatsen.sort(),
         ).toStrictEqual(['4', '5']);
+    });
+
+    it('Ondernemers kunnen eerst uitbreiden naar 2 plaatsen, voordat anderen mogen uitbreiden naar meer plaatsen', () => {
+        /*
+         * Scenario:
+         * - 3 marktplaatsen
+         * - 1 ondernemer met een vaste plek op de middelste kraam
+         * - de ondernemer heeft een voorkeur voor kraam 2, daarna 1, daarna 3
+         */
+        const markt = marktScenario(({ ondernemer, marktplaats, voorkeur }) => ({
+            ondernemers: [
+                ondernemer({ voorkeur: { aantalPlaatsen: 3 }, plaatsen: ['3', '4'], status: 'vpl' }),
+                ondernemer({ voorkeur: { aantalPlaatsen: 2 } }),
+            ],
+            marktplaatsen: [marktplaats(), marktplaats(), marktplaats(), marktplaats()],
+            voorkeuren: [
+                voorkeur({ sollicitatieNummer: 1, plaatsId: '3', priority: FIRST_CHOICE }),
+                voorkeur({ sollicitatieNummer: 1, plaatsId: '4', priority: SECOND_CHOICE }),
+                voorkeur({ sollicitatieNummer: 1, plaatsId: '2', priority: THIRD_CHOICE }),
+                voorkeur({ sollicitatieNummer: 2, plaatsId: '1', priority: FIRST_CHOICE }),
+                voorkeur({ sollicitatieNummer: 2, plaatsId: '2', priority: SECOND_CHOICE }),
+            ],
+        }));
+
+        const indeling = calcToewijzingen(markt);
+
+        expect(indeling.toewijzingen.length).toBe(2);
+        expect(indeling.afwijzingen.length).toBe(0);
+        expect(
+            indeling.toewijzingen
+                .find(({ ondernemer: { sollicitatieNummer } }) => sollicitatieNummer === 1)
+                .plaatsen.sort(),
+        ).toStrictEqual(['3', '4']);
+        expect(
+            indeling.toewijzingen
+                .find(({ ondernemer: { sollicitatieNummer } }) => sollicitatieNummer === 2)
+                .plaatsen.sort(),
+        ).toStrictEqual(['1', '2']);
     });
 });
 

src/indeling.ts
@@ -18,6 +18,8 @@ const VOORKEUR_MINIMUM_PRIORITY = 0;
 const intersects = (a: any[] = [], b: any[] = []) =>
     a.some(value => b.includes(value)) || b.some(value => a.includes(value));
 
+const intersection = (a: any[] = [], b: any[] = []) => a.filter(value => b.includes(value));
+
 const isVast = (status: DeelnemerStatus): boolean => status === 'vpl' || status === 'vkk';
 
 /*
@@ -223,7 +225,7 @@ const assignPlaats = (
     };
 
     if (existingToewijzing) {
-        console.log(`Ondernemer is reeds toegwezen aan plaats(en): ${existingToewijzing.plaatsen.join('/')}`);
+        console.log(`Ondernemer is reeds toegwezen aan plaats(en): ${existingToewijzing.plaatsen.join(', ')}`);
 
         if (conflictResolution === 'merge') {
             newToewijzing = mergeToewijzing(existingToewijzing, newToewijzing);
@@ -305,17 +307,30 @@ const assignUitbreiding = (indeling: IMarktindeling, toewijzing: IToewijzing): I
         `Ondernemer ${
             ondernemer.erkenningsNummer
         } wil ${aantalPlaatsen} plaatsen, en heeft nu ${currentPlaatsen} plaats(en)`,
+        toewijzing,
     );
 
-    if (aantalPlaatsen > currentPlaatsen) {
+    const removeFromQueue = (indeling: IMarktindeling, toewijzing: IToewijzing) => ({
+        ...indeling,
+        expansionQueue: (indeling.expansionQueue || []).filter(t => t !== toewijzing),
+    });
+
+    const replaceInQueue = (indeling: IMarktindeling, current: IToewijzing, replacement: IToewijzing) => ({
+        ...indeling,
+        expansionQueue: indeling.expansionQueue.map(item => (item === current ? replacement : item)),
+    });
+
+    if (currentPlaatsen < aantalPlaatsen) {
         const adjacent = getAdjacentPlaatsenForMultiple(indeling, plaatsen);
+        const openAdjacent = intersection(adjacent, indeling.openPlaatsen);
+
         console.log(
-            `Er zijn ${adjacent.length} aanliggende plaatsen voor ${plaatsen}: ${adjacent
-                .map(({ plaatsId }) => plaatsId)
-                .join('/')}`,
+            `Er zijn ${openAdjacent.length} open van de ${adjacent.length} aanliggende plaatsen voor ${plaatsen.join(
+                ', ',
+            )}: ${openAdjacent.map(({ plaatsId }) => plaatsId).join(', ')}`,
         );
 
-        const uitbreidingPlaats = findOptimalSpot(ondernemer, indeling.voorkeuren, adjacent, indeling);
+        const uitbreidingPlaats = findOptimalSpot(ondernemer, indeling.voorkeuren, openAdjacent, indeling);
 
         if (uitbreidingPlaats) {
             // Remove vrije plaats
@@ -329,16 +344,19 @@ const assignUitbreiding = (indeling: IMarktindeling, toewijzing: IToewijzing): I
                 plaatsen: [...toewijzing.plaatsen, uitbreidingPlaats.plaatsId],
             };
 
-            console.log(`Ondernemer kan uitbreiden naar ${uitbreidingPlaats.plaatsId}`, toewijzing, uitbreiding);
+            console.log(
+                `Ondernemer ${ondernemer.erkenningsNummer} kan uitbreiden naar ${uitbreidingPlaats.plaatsId}`,
+                toewijzing,
+                uitbreiding,
+            );
 
             indeling = replaceToewijzing(indeling, toewijzing, uitbreiding);
 
             if (currentPlaatsen + 1 < aantalPlaatsen) {
                 // Do You Want More?!!!??!
-                indeling = {
-                    ...indeling,
-                    expansionQueue: [...(indeling.expansionQueue || []), toewijzing],
-                };
+                indeling = replaceInQueue(indeling, toewijzing, uitbreiding);
+            } else {
+                indeling = removeFromQueue(indeling, toewijzing);
             }
             // TODO: Merge `toewijzing` and `uitbreiding` objects, add to `indeling`
         } else {
@@ -346,6 +364,7 @@ const assignUitbreiding = (indeling: IMarktindeling, toewijzing: IToewijzing): I
                 `Geen uitbreiding van ${currentPlaatsen} naar ${currentPlaatsen +
                     1} plaatsen voor ondernemer ${erkenningsNummer}`,
             );
+            indeling = removeFromQueue(indeling, toewijzing);
         }
     }
 
@@ -487,6 +506,11 @@ const calcToewijzingen = (markt: IMarkt & IMarktindelingSeed): IMarktindeling =>
         ...markt,
         toewijzingQueue: [...aanwezigen],
         expansionQueue: [],
+        expansionIteration: 1,
+        expansionLimit: Math.min(
+            Number.isFinite(markt.expansionLimit) ? markt.expansionLimit : Infinity,
+            markt.marktplaatsen.length,
+        ),
         afwijzingen: [],
         toewijzingen: [],
         openPlaatsen: [...marktplaatsen.filter(plaats => !plaats.inactive)],
@@ -567,9 +591,9 @@ const calcToewijzingen = (markt: IMarkt & IMarktindelingSeed): IMarktindeling =>
         console.log(
             `Voor ondernemer ${ondernemer.erkenningsNummer} zijn er nog ${
                 betereVoorkeuren.length
-            } plaatsen die meer gewenst zijn (van de in totaal ${voorkeuren.length} voorkeuren, ${voorkeuren.map(
-                voorkeur => voorkeur.plaatsId,
-            )}), en daarvan zijn nog ${beterePlaatsen.length} vrij`,
+            } plaatsen die meer gewenst zijn (van de in totaal ${voorkeuren.length} voorkeuren: ${voorkeuren
+                .map(voorkeur => voorkeur.plaatsId)
+                .join(', ')}), en daarvan zijn nog ${beterePlaatsen.length} vrij`,
         );
 
         state = findPlaats(state, ondernemer, 0, [], beterePlaatsen, 'ignore');
@@ -577,19 +601,54 @@ const calcToewijzingen = (markt: IMarkt & IMarktindelingSeed): IMarktindeling =>
         return state;
     }, indeling);
 
-    const expansionQueue = indeling.toewijzingen.filter(
-        ({ ondernemer }) => !!ondernemer.voorkeur && ondernemer.voorkeur.aantalPlaatsen > 1,
-    );
+    indeling = {
+        ...indeling,
+        expansionQueue: indeling.toewijzingen.filter(
+            ({ ondernemer }) =>
+                !!ondernemer.voorkeur && ondernemer.voorkeur.aantalPlaatsen > indeling.expansionIteration,
+        ),
+    };
 
-    indeling = expansionQueue.reduce(assignUitbreiding, indeling);
+    while (indeling.expansionIteration < indeling.expansionLimit) {
+        console.log(
+            `Uitbreidingsronde naar ${indeling.expansionIteration + 1} plaatsen (later tot maximaal ${
+                indeling.expansionLimit
+            })`,
+        );
 
-    /*
-    console.warn(
-        `Na mogelijkheid tot uitbreiding zijn er nog ${
-            indeling.expansionQueue.map(({ ondernemer }) => ondernemer).reduce(unique, []).length
-        } ondernemers die meer plaatsen willen`,
-    );
-    */
+        // TODO: Remove items from expansion queue for which expansion was impossible,
+        // skip those in the next iteration.
+
+        indeling = indeling.expansionQueue
+            .filter(toewijzing => {
+                const { ondernemer, plaatsen } = toewijzing;
+                const { aantalPlaatsen } = ondernemer.voorkeur;
+                const currentPlaatsen = plaatsen.length;
+
+                console.log(
+                    `Ondernemer ${
+                        ondernemer.erkenningsNummer
+                    } wil ${aantalPlaatsen} plaatsen, en heeft nu ${currentPlaatsen} plaats(en)`,
+                    toewijzing,
+                );
+
+                return currentPlaatsen < Math.min(aantalPlaatsen, indeling.expansionIteration, indeling.expansionLimit);
+            })
+            .reduce(assignUitbreiding, indeling);
+
+        /*
+        console.warn(
+            `Na mogelijkheid tot uitbreiding ${indeling.expansionIteration} zijn er nog ${
+                indeling.expansionQueue.map(({ ondernemer }) => ondernemer).reduce(unique, []).length
+            } ondernemers die meer plaatsen willen`,
+        );
+        */
+
+        indeling = {
+            ...indeling,
+            expansionIteration: indeling.expansionIteration + 1,
+        };
+    }
 
     indeling = indeling.expansionQueue.reduce(assignUitbreiding, indeling);
 

src/markt.model.ts
@@ -34,6 +34,7 @@ export interface IMarkt {
     marktplaatsen: IMarktplaats[];
     voorkeuren: IPlaatsvoorkeur[];
     ondernemers: IMarktondernemer[];
+    expansionLimit: number;
 }
 
 export interface IMarktindelingSeed {
@@ -110,6 +111,7 @@ export interface IMarktindelingState extends IMarkt {
     toewijzingQueue: IMarktondernemer[];
     openPlaatsen: IMarktplaats[];
     expansionQueue: IToewijzing[];
+    expansionIteration: number;
 }
 
 export interface IMarktindeling extends IMarkt, IMarktindelingSeed, IMarktindelingState {

commit 2d7011ca54d52d61f9c1dd762f199f333aed138f

Fix VPH uitbreidingen regression from c480354

src/pakjekraam-api.js
@@ -190,7 +190,10 @@ const getIndelingslijstInput = (token, marktId, marktDate) => {
     const enrichedOndernemers = Promise.all([ondernemersPromise, voorkeurenPromise]).then(([ondernemers, voorkeuren]) =>
         ondernemers.map(ondernemer => ({
             ...ondernemer,
-            voorkeur: voorkeuren.find(voorkeur => voorkeur.erkenningsNummer === ondernemer.erkenningsNummer),
+            voorkeur: {
+                ...ondernemer.voorkeur,
+                ...voorkeuren.find(voorkeur => voorkeur.erkenningsNummer === ondernemer.erkenningsNummer),
+            },
         })),
     );
 